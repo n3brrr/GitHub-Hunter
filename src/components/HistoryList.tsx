7dbef6756977fcb60c5a03656de4c25f15158325
Ruben <neburtl837@gmail.com>
2026-02-05 13:43:33 +0100

    Refactor code

--- src/components/HistoryList.tsx
+++ src/components/HistoryList.tsx
@@ -3,16 +3,24 @@ import type { GithubUser } from "../types";
 export default function HistoryList({
   history,
   setHistory,
+  onSelectUser,
 }: {
   history: GithubUser[];
   setHistory: (history: GithubUser[]) => void;
+  onSelectUser: (username: string) => void;
 }) {
   return (
     <div className="flex flex-col gap-4 mt-10 border-t-2 border-gray-200 pt-10 justify-center max-w-lg mx-auto">
+      {history.length === 0 && (
+        <p className="text-center text-gray-500">
+          Search user to add to history
+        </p>
+      )}
       <h2 className="text-2xl font-bold">History</h2>
       {history.map((user) => (
         <div
           key={user.id}
+          onClick={() => onSelectUser(user.login)}
           className="flex items-center gap-4 p-2 hover:mb-4 hover:shadow-lg hover:shadow-blue-500/80 hover:scale-105 transition-all duration-300 rounded-xl"
         >
           <img
@@ -20,7 +28,9 @@ export default function HistoryList({
             alt={user.login}
             className="w-10 h-10 rounded-full"
           />
-          <p>{user.login}</p>
+          <p className="cursor-pointer hover:text-blue-500 transition-all duration-300">
+            {user.login}
+          </p>
         </div>
       ))}
       {history.length > 0 && (
