de6ef28cb5120854eb41c133974c518161a9c368
Ruben <neburtl837@gmail.com>
2026-02-05 09:10:17 +0100

    feat: Introduce UserCard component for displaying GitHub user information and add corresponding loading spinner styles.

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,22 +1,33 @@
 import Loading from "./Loading";
+import type { GithubUser } from "../types";
 
-export default function UserCard({ user, loading, error }: any) {
+export default function UserCard({ user, loading, error }: GithubUser) {
   if (loading) return <Loading />;
-  if (error) return <p>Error: {error.message}</p>;
+  if (error) return <p className="text-red-500">Error: {error}</p>;
+  if (user === null) return <p className="text-center mt-5">Ready to Hunt?</p>;
 
   return (
-    <div>
-      <p>{user.name}</p>
-      <p>{user.location}</p>
-      <p>{user.company}</p>
-      <p>{user.blog}</p>
-      <p>{user.email}</p>
-      <p>{user.public_repos}</p>
-      <p>{user.public_gists}</p>
-      <p>{user.followers}</p>
-      <p>{user.following}</p>
-      <p>{user.created_at}</p>
-      <p>{user.updated_at}</p>
+    <div className="shadow-lg rounded-lg p-4 border-2 border-gray-200 flex flex-col items-center">
+      <img
+        src={user.avatar_url}
+        alt={user.name || user.login}
+        className="w-32 h-32 rounded-full"
+      />
+      <p className="text-lg font-bold text-white">{user.name || user.login}</p>
+      <p className="text-white">
+        {user.location && `Location: ${user.location}`}
+      </p>
+      <p className="text-white">{user.company && `Company: ${user.company}`}</p>
+      <p className="text-white">{user.blog && `Blog: ${user.blog}`}</p>
+      <p className="text-white">{user.email && `Email: ${user.email}`}</p>
+      <a
+        href={user.html_url}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-white bg-blue-500 p-2 rounded-lg mt-2"
+      >
+        View Profile
+      </a>
     </div>
   );
 }
